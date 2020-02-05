16c3289a08c32a44ec152ed657eb46380d5894cf
izak270 <izak270@gmail.com>
2020-02-05 16:32:46 +0200

    stage 2 complete

--- facts/models.py
+++ facts/models.py
@@ -5,21 +5,25 @@ from django.utils import timezone
 
 
 class Artist(models.Model):
-    artist_first_name = models.CharField(max_length=200)
-    artist_last_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f'{self.artist_first_name} {self.artist_last_name}'
+        return f'{self.name}'
 
 
 class Song(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
-    song_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Facts(models.Model):
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     fact = models.TextField()
-    author = models.CharField(max_length=100)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.song}'
